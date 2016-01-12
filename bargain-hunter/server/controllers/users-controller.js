2f404d38606a7592cd23e3bbd197e69b02166dfa
DennyGD <denny.dimitrova@abv.bg>
2016-01-12 22:46:17 +0200

    server-side mail validation

--- bargain-hunter/server/controllers/users-controller.js
+++ bargain-hunter/server/controllers/users-controller.js
@@ -117,7 +117,17 @@ function checkIfValidUser(user) {
         return false;
     }
 
+    if (!user.email || !(checkIfValidEmail(user.email))) {
+        return false;
+    }
+
     return true;
+}
+
+function checkIfValidEmail(email) {
+    if (email.length === 0) return false;
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
 
 module.exports = {
