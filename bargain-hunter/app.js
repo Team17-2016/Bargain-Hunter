c7607ee339bef91fa8463650402c9c8cb946225f
damy90 <dany90@gmail.com>
2016-01-12 19:25:33 +0200

    unit tests for server: homepage and non existing routes

--- bargain-hunter/app.js
+++ bargain-hunter/app.js
@@ -1,62 +1,74 @@
-'use strict';
+function makeServer() {
+    'use strict';
 
-const PORT = 65442,
-    LOGGER_FORMAT = 'combined';
+    const PORT = 65442,
+        LOGGER_FORMAT = 'combined';
 
-let express = require('express'),
-    bodyParser = require('body-parser'),
-    logger = require('morgan'),
-    passport = require('passport'),
-    expressSession = require('express-session'),
-    db = require('./server/config/db'),
-    app = express();
+    let express = require('express'),
+        bodyParser = require('body-parser'),
+        logger = require('morgan'),
+        passport = require('passport'),
+        expressSession = require('express-session'),
+        db = require('./server/config/db'),
+        app = express();
 
-db.init();
+    db.init();
 
-// TODO remove/edit when project is ready
-app.use(logger(LOGGER_FORMAT));
+    // TODO remove/edit when project is ready
+    app.use(logger(LOGGER_FORMAT));
 
-app.use(expressSession({ secret: 'coffee', resave: false, saveUninitialized: false }));
+    app.use(expressSession({
+        secret: 'coffee',
+        resave: false,
+        saveUninitialized: false
+    }));
 
-// Setup Body-Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+    // Setup Body-Parser
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-// Setup static files access
-app.use("/public", express.static(__dirname + '/public/'));
+    // Setup static files access
+    app.use("/public", express.static(__dirname + '/public/'));
 
-// Setup database models
-require('./server/models');
+    // Setup database models
+    require('./server/models');
 
-// Setup Jade
-require('./server/config/jade-config')(app);
+    // Setup Jade
+    require('./server/config/jade-config')(app);
 
-require('./server/config/passport-config')();
+    require('./server/config/passport-config')();
 
-// Setup routers
-require('./server/routers')(app, express);
+    // Setup routers
+    require('./server/routers')(app, express);
 
-// Middleware for last-resort error-handling
-app.use(function(err, req, res, next) {
-    if (err) {
-        res.status(err.status || 500)
-            .json({
-                message: err.message
-            });
-    }
-});
+    // Middleware for last-resort error-handling
+    app.use(function (err, req, res, next) {
+        if (err) {
+            res.status(err.status || 500)
+                .json({
+                    message: err.message
+                });
+        }
+    });
 
-// Start server event listener
-app.listen(PORT, function (err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
+    // Start server event listener
+    app.listen(PORT, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
 
-    console.log('Server running at localhost:${PORT}');
-});
+        console.log(`Server running at localhost:${PORT}`);
+    });
+
+    return app;
+}
+
+makeServer();
+
+module.exports = makeServer;
